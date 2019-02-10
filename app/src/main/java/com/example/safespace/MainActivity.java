02edc5072ff48a51cacb7f681032b7da98b543eb
Nikitha Preetham <nikithalal@gmail.com>
2019-02-09 20:21:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/safespace/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.safespace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
